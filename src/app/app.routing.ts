547ae2589d67b375a93f3951fa3695796f1fb8d6
Dicky <dbudianto9@gmail.com>
2019-11-16 20:42:16 +0700

    backing up unused file

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -11,10 +11,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { TableListComponent } from './table-list/table-list.component';
 import { TypographyComponent } from './typography/typography.component';
-import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
 import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
 
 const routes: Routes =[
   {
