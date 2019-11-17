50211dfa89de38943dd6603a2ecaeb7cf3bc8810
Dicky <dbudianto9@gmail.com>
2019-11-17 13:10:39 +0700

    schedule form

--- src/app/form/schedule/formSchedule.component.ts
+++ src/app/form/schedule/formSchedule.component.ts
@@ -1,9 +1,26 @@
 import {Component} from '@angular/core';
 
+export interface Coach {
+  name: string;
+  value: string;
+}
+
 @Component({
   selector: 'formSchedule',
   templateUrl: 'formSchedule.component.html',
   styleUrls: ['formSchedule.component.css'],
 })
 
-export class FormSchedule {}
+
+export class FormSchedule {
+  coaches: Coach[] = [
+    {name: 'Coach 1', value: '1'},
+    {name: 'Coach 2', value: '2'},
+    {name: 'Coach 3', value: '3'},
+    {name: 'Coach 4', value: '4'},
+  ];
+
+  submitClass(){
+    console.log('ayo');
+  }
+}
